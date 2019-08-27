020ae9624ed7dda9b27ee5200120c7407cdfe76a
Babichev Maxim <info@babichev.net>
2019-08-27 14:35:05 +0300

    Update XHProfService.php

--- src/Services/XHProfService.php
+++ src/Services/XHProfService.php
@@ -5,7 +5,8 @@ namespace Bavix\XHProf\Services;
 use Bavix\XHProf\Providers\ProviderInterface;
 use Bavix\XHProf\Providers\XHProfProvider;
 use Bavix\XHProf\Providers\EmptyProvider;
-
+use function mt_getrandmax;
+use function mt_rand;
 
 class XHProfService
 {
@@ -20,7 +21,8 @@ class XHProfService
      */
     public function __construct()
     {
-        if (extension_loaded('xhprof')) {
+        $freq = config('xhprof.freq', 0.1);
+        if (extension_loaded('xhprof') && ($freq >= (mt_rand() / mt_getrandmax()))) {
             $this->provider = new XHProfProvider();
         } else {
             $this->provider = new EmptyProvider();
